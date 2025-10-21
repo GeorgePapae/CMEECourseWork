c97dc5c04648e4399f1f5cf5fd6fbf34e6c7c6d3
George <giorgos.papaeracleous22@imperial.ac.uk>
2025-10-21 11:30:12 +0100

    Continuing R coursework

--- week3/code/Florida.R
+++ week3/code/Florida.R
@@ -5,17 +5,20 @@ load("../data/KeyWestAnnualMeanTemperature.RData")
 ls()
 class(ats)
 head(ats)
-plot(ats)
+plot(ats$Year, ats$Temp, xlab = "Year", ylab = "Temperature (°C)")
 # Calculate the observed correlation between the data
 obs_cor <- cor(ats$Year, ats$Temp)
 obs_cor
+
 # Set number of permutations and create an empty vector
 num_perms <- 10000
 perm_cor <- c()
+
 # Run the loop for num_perms times while keeping the years and sampling the temperature values
 for (i in 1:num_perms){
     perm_cor[i] <- cor(ats$Year, sample(ats$Temp))
 }
+
 # Find the fraction of correlation values from permutational analysis
 # that are bigger than our observed correlation value
 p_value <- sum (perm_cor >= obs_cor)/ num_perms
